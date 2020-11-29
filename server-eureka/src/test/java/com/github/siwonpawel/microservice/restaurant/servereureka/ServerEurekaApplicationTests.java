279ee613c1b1afbaabb3492817f7685ff608e898
siwonpawel <siwonpawel@gmail.com>
2020-11-29 20:34:46 +0100

    EurekaServer configuration

---
+++ server-eureka/src/test/java/com/github/siwonpawel/microservice/restaurant/servereureka/ServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.siwonpawel.microservice.restaurant.servereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
